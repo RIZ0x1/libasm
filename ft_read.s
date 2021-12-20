b8725d72501d454d90e6ceb3d7cdfaf80b575630
RIZ0x1 <riz1.kibiev@gmail.com>
2021-12-20 04:10:14 +0300

    Read in progress

---
+++ ft_read.s
@@ -0,0 +1,10 @@
+section .text
+global _ft_read
+
+_ft_read:
+	mov	rax, 0	; move system call number (read) to RAX
+	int	0x80	; Knock-knock, Kernel. fd, buf and flags are in RDI, RSI, RDX registers respectively
+	cmp	rax, 0	; comparing returned value with 0
+	jl	error	; if it less than 0 RIP jumps to error label
+error:
+	
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-int	ft_test(void);
-
-int	main(void)
-{
-	ft_test();
-}
