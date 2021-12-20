9767aa0c9b6525b105f481ac287f388e8d6db6e8
RIZ0x1 <riz1.kibiev@gmail.com>
2021-12-20 11:00:28 +0300

    shie

--- ft_read.s
+++ ft_read.s
@@ -6,5 +6,11 @@ _ft_read:
 	int	0x80	; Knock-knock, Kernel. fd, buf and flags are in RDI, RSI, RDX registers respectively
 	cmp	rax, 0	; comparing returned value with 0
 	jl	error	; if it less than 0 RIP jumps to error label
+	jmp	return	; exit from func
 error:
-	
+	push	rax
+	call	___error
+	pop	rax
+	mov	rax, -1
+return:
+	ret
