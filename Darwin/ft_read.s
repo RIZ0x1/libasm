8108ee497ab2773ec350aa7076dc1765d050a407
RIZ1 <62781747+RIZ0x1@users.noreply.github.com>
2022-01-20 18:25:24 +0300

    Not compile typo fixed

--- Darwin/ft_read.s
+++ Darwin/ft_read.s
@@ -3,7 +3,7 @@ global _ft_read
 extern ___error
 
 _ft_read:
-	mov		rax, 0x2000003. ; move system call number (read) to RAX
+	mov		rax, 0x2000003  ; move system call number (read) to RAX
 	syscall	                        ; Knock-knock, Kernel. fd, buf and flags are in RDI, RSI, RDX registers respectively
 	jc		error           ; if (syscall() < 0) { goto error; } Carry flag - checks the first left bit (10000000 & x)
 	ret                             ; else { goto return; }
